dd587391816cdcc985551333578759129d21f93b
antciccone <aciccone@bu.edu>
2017-04-26 14:41:34 -0400

    initialize princess and some sudo code

--- saves_princess_one/lib/princess.rb
+++ saves_princess_one/lib/princess.rb
@@ -1,7 +1,18 @@
 class Princess
 
-def displayPathtoPrincess(n,grid)
+  attr_reader :grid
+  def initialize(size, grid)
+    @grid = grid
+  end
+
+  def displayPathtoPrincess
+
+  end
 
 end
 
-end
+#get coordinates of princess and mario
+#once coordinates are located calculate disance between mario and princess
+#possibly store their coordinates in an array
+#once you find the distance between both - use Directions up, down, left, right
+#initialize with grid - easier to access from all methods
--- saves_princess_one/spec/princess_spec.rb
+++ saves_princess_one/spec/princess_spec.rb
@@ -3,10 +3,11 @@ require_relative '../lib/princess'
 
 describe "Princess" do
   context "can initialize with specific data" do
-    it "returns " do
+    it "returns the grid" do
 
-      Princess.new.displayPathtoPrincess(1,2)
+      princess = Princess.new(3,["---", "-m-", "p--"])
 
+      expect(princess.grid).to eq(["---", "-m-", "p--"])
     end
   end
 end
