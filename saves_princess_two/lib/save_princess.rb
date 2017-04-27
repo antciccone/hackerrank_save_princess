37ec50c164ca516f2ccb4d119a449daeb7c520fe
antciccone <aciccone@bu.edu>
2017-04-26 20:25:13 -0400

    add first test set up and sudo code

--- saves_princess_two/lib/save_princess.rb
+++ saves_princess_two/lib/save_princess.rb
@@ -1,4 +1,14 @@
 class Princess
+  def initialize(n, row, column, grid)
+    @grid = grid
+    @mario_coordinates = [row, column]
+    @princess_coordinates = []
+  end
 
+end
 
-end 
+# first glance at second problem is that it looks at similar to first.
+# Differences are marios position is given right away and only output next move
+# follow similar logic of initializing with mario and princes
+# cooridnates method will only grab the princess location
+# outputting princess location will only print the first move
--- saves_princess_two/spec/save_princess_spec.rb
+++ saves_princess_two/spec/save_princess_spec.rb
@@ -4,6 +4,8 @@ require_relative '../lib/princess'
 describe "Princess" do
 
   it "creates an instance of the Princess class" do
+    princess = Princess.new(3,1,1,["---", "-m-", "p--"])
 
+    expect(princess.class).to eq(Princess)
   end
 end
