38cfe788d4a575959a22ea0954123065cf2d05c2
antciccone <aciccone@bu.edu>
2017-04-26 20:13:08 -0400

    set up of princess two challenge

---
+++ saves_princess_two/lib/save_princess.rb
@@ -0,0 +1,4 @@
+class Princess
+
+
+end 
---
+++ saves_princess_two/spec/save_princess_spec.rb
@@ -0,0 +1,9 @@
+require 'rspec'
+require_relative '../lib/princess'
+
+describe "Princess" do
+
+  it "creates an instance of the Princess class" do
+
+  end
+end
